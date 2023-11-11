6efb5fcff67ef2365b1a13e54b7eae9e3c406420
Anatolii Iumashev <a.yumashev@gmail.com>
2023-11-11 12:55:29 +0300

    add experiment with data object for better autocomplete

--- blocks/post-widget-telegram/block.php
+++ blocks/post-widget-telegram/block.php
@@ -6,13 +6,24 @@
  * @var  string $context Preview context [editor,frontend].
  */
 
-$url = $attributes['url'];
-$url_parts = wp_parse_url($url);
-$path = ltrim( $url_parts['path'], '/' );
 
-// echo 1;
+$data = new class($attributes){
+    public $path = null;
+
+    function __construct($attributes){
+        $url = $attributes['url'];
+        $url_parts = wp_parse_url($url);
+        $this->path = ltrim( $url_parts['path'], '/' );
+    }
+};
+
+if(empty($data->path)){
+    echo "no path for Telegram post";
+    return;
+}
+
 ?>
 <div class="lb-telegram-widget">
-    <script async src="https://telegram.org/js/telegram-widget.js?15" data-telegram-post="<?= $path ?>" data-width="100%" data-userpic="false">
+    <script async src="https://telegram.org/js/telegram-widget.js?15" data-telegram-post="<?= $data->path ?>" data-width="100%" data-userpic="false">
     </script>
 </div>
